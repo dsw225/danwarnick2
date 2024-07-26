033b7c2d37ce10daa58f2deeeede48431c35e2ac
Dan Warnick <dsw225@lehigh.edu>
2024-07-25 21:11:16 -0400

    Update for future need to scale

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import './index.css'; // Import the CSS file
 import { ContentWrapper } from '../components/Navbar/NavbarElements';
 import TypewriterComponent from '../components/Typewriter/TypewriterComponent';
 import slapImg from '../images/slapdisplay.jpg';
-import gdImg from '../images/MAwork.jpg';
+import bpImg from '../images/BreakPointLogo3.png';
 import pgaImg from '../images/PGAalt.jpg';
 import dwImg from '../images/DanWarnick1.jpg';
 
@@ -21,29 +21,29 @@ const Home = () => {
 							<h1><TypewriterComponent /></h1>
 						</div>
 						<div className='text-primary'>
-							<h2>Student, <br/>Engineer, <br/>Aspiring Developer.</h2>
+							<h2>Student, <br/>Aspiring Developer.</h2>
 							</div>
 					</div>
 				</div>
 				<div className='second-block' id="about">
 					<div className='hello-container'>
-						<h2>Hi, I’m Dan. Nice to meet you.</h2>
+						<h2>Hi, I’m Dan.</h2>
 					</div>
 					<div className='hello-text'>
 						<p>Currently a Student at Lehigh University P.C. Rossin College of Engineering and Applied Science, majoring in Computer Science on the Artificial Intelligence Track, with minors in Graphic Design and Data Science.
-							My academic focus reflects a strong interest in both front-end development and full-stack development, with particular passions for UI/UX design, and AI.
+							My academic focus reflects a strong interest in both data-science, and full-stack development, with particular passions for machine learning, and AI.
 						</p>
 					</div>
 					<div className='more-text'>
 						<h2>My Skills</h2>
 					</div>
 					<div className='skills-container'>
+						<div className='skills-bubble'>Python</div>
 						<div className='skills-bubble'>Java</div>
 						<div className='skills-bubble'>HTML</div>
 						<div className='skills-bubble'>CSS</div>
 						<div className='skills-bubble'>React</div>
 						<div className='skills-bubble'>JavaScript</div>
-						<div className='skills-bubble'>Python</div>
 						<div className='skills-bubble'>Graphic Design</div>
 						<div className='skills-bubble'>Adobe Photoshop</div>
 						<div className='skills-bubble'>Adobe Illustrator</div>
@@ -59,22 +59,22 @@ const Home = () => {
 						<h2>My Recent Work</h2>
 					</div>
 					<div className='projects-text'>
-						<p>Here are a few past design & coding projects I've worked on.</p>
+						<p>Here are a few current & past projects I've worked on/am working on.</p>
 					</div>
 					<div className='projects-bubble-container'>
 						<div className='projects-bubble' onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
 							<div className="image-container">
-								<img src={slapImg} alt="Zoomed Image" className={`zoom-img ${isHovered ? 'hovered' : ''}`} />
+								<img src={bpImg} alt="Zoomed Image" className={`zoom-img ${isHovered ? 'hovered' : ''}`} />
 								<div className="image-text">
-									With team, created a fully functioning web + mobile Twitter clone utilizing React, Dart, and PostgreSQL.
+									<a href="https://breakpoint.bet/" target="_blank">ATP & WTA Tennis Prediction Model</a> , to analyze win odds, and grade probable outcomes vs vegas.
 								</div>
 							</div>
 						</div>
 						<div className='projects-bubble' onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
 							<div className="image-container">
-								<img src={gdImg} alt="Zoomed Image" className={`zoom-img ${isHovered ? 'hovered' : ''}`} />
+								<img src={slapImg} alt="Zoomed Image" className={`zoom-img ${isHovered ? 'hovered' : ''}`} />
 								<div className="image-text">
-									Miscellaneous work from GD Classes + Internship + Freelance.
+									With team, created a fully functioning web + mobile Twitter clone utilizing React, Dart, and PostgreSQL.
 								</div>
 							</div>
 						</div>
@@ -98,13 +98,13 @@ const Home = () => {
 				</div>
 				<div className='fourth-block' id="contact">
 					<div className='hello-container'>
-						<h2>Contact Me!</h2>
+						<h2>Contact Me</h2>
 					</div>
 					<div className='hello-text' style={{ lineHeight: '3.5rem' }}>
 						<ul>
 							<li>Email: <a href="mailto:dsw225@lehigh.edu" target="_blank">dsw225@lehigh.edu</a></li>
 							<li>LinkedIn: <a href="https://www.linkedin.com/in/dan-warnick/" target="_blank">Dan Warnick</a></li>
-							<li>My Resume: <a href="https://drive.google.com/file/d/1nbo-5j95yVGYlddiUgK99ej3Q8GP5Mky/view?usp=sharing" target="_blank">Resume</a></li>
+							<li>My Resume: <a href="https://drive.google.com/file/d/1SYO2zLX9Z-1yOsgA0NZYuLLpYN_b19DC/view?usp=sharing" target="_blank">Resume</a></li>
 							<li>GitHub: <a href="https://github.com/dsw225/" target="_blank">@dsw225</a></li>
 						</ul>
 					</div>
